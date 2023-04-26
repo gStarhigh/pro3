66850209257b4efe4dc82dd855e20891e7cbb599
Gustaf Starhög <gustafstar@hotmail.com>
2023-04-26 06:45:13 +0000

    Added API's and tested the access to google sheet successfully with print statements

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# imports
+import gspread
+from google.oauth2.service_account import Credentials
+
+#Scope
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+ACCOUNT_SHEET = GSPREAD_CLIENT.open("budget_accounts")
+DATA_SHEET = GSPREAD_CLIENT.open("budget_data")
+
+budget_accounts = ACCOUNT_SHEET.worksheet("tab1")
+budget_data = DATA_SHEET.worksheet("tab1")
+
+account_creds = budget_accounts.get_all_values()
+print(account_creds)
+
+budget_info = budget_data.get_all_values()
+print(budget_info)
+
+
