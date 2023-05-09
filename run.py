58ba82c4850fb33a15a8fcfc005d4127fc520450
Gustaf Starhög <gustafstar@hotmail.com>
2023-05-09 08:11:50 +0000

    removed whitespace with info from the python linter

--- run.py
+++ run.py
@@ -45,25 +45,25 @@ def get_account_details():
     print("Welcome to your Monthly budget application. \n")
     print("If you are a first time user, choose an account name, "
           "it must be unique. \n")
-    print("If you are a returning user, please enter your existing " 
+    print("If you are a returning user, please enter your existing "
           "account name below. \n")
     account_name = input("Enter your account name: \n")
     print(f"Checking your account name '{account_name}'..")
-    
+
     # Check if the account name already exists in the sheet
     account_names = [row[0] for row in account_creds]
     saved_pin = None
     if account_name in account_names:
-        print(f"The account name {account_name} was matched against " 
+        print(f"The account name {account_name} was matched against "
               "the database. Please continue")
         for row in account_creds:
             if row[0] == account_name:
                 saved_pin = row[1]
                 break
     else:
-        print(f"The account name: {account_name} was not found, " 
+        print(f"The account name: {account_name} was not found, "
               "creating a new Account")
-        
+
         # Asks the user for the pincode and ensure the length is 4 numbers
         while True:
             account_pin = input("Enter your pincode(4 numbers): \n")
@@ -71,12 +71,12 @@ def get_account_details():
                 break
             else:
                 print("The pincode must be 4 numbers. Please try again")
-        
+
         # Encode the pincode
         account_pin = account_pin.encode("utf-8")
         # Encrypt the stored pincode
         hashed_pin = bcrypt.hashpw(account_pin, bcrypt.gensalt(10))
-        
+
         # Append the account name and hashed pincode to the sheet
         # if the user does not already exist
         new_row = [account_name, hashed_pin.decode()]
@@ -84,7 +84,7 @@ def get_account_details():
         print(f"New account '{account_name}' was created successfully")
         saved_pin = hashed_pin.decode()
         return account_name, saved_pin
-    
+
     while True:
         # Compare the stored password with the entered password
         account_pin = input("Enter your pincode(4 numbers): \n")
@@ -100,23 +100,23 @@ def get_account_details():
         else:
             print("The pincode must be 4 numbers, not letters. "
                   "Please try again.")
-                
+
     return account_name, saved_pin
 
 
 # Get the budget amount from the user and validate the input
 def get_budget(account_name, saved_pin):
-    """ 
+    """
     Get the current date and month from datetime.
-    Get the budget month from the user and validate it against datetime. 
-    Get the budget amount from the user and validate the input. 
+    Get the budget month from the user and validate it against datetime.
+    Get the budget amount from the user and validate the input.
     Saves the budget month and the budget amount to google sheets.
     """
     current_month = datetime.date.today().strftime("%B")
     today = datetime.date.today()
     next_month = (today + datetime.timedelta(days=31)).strftime("%B")
     valid_months = [current_month, next_month]
-    
+
     while True:
         print(f"You can only choose from these options: {valid_months}")
         budget_month = input("Enter the month for the budget: \n")
@@ -126,7 +126,7 @@ def get_budget(account_name, saved_pin):
         else:
             print(f"You can only choose from either {current_month} "
                   f" or {next_month}. Please try again.")
-        
+
     while True:
         try:
             total_budget = int(input("Enter your total budget: \n"))
@@ -142,8 +142,8 @@ def get_budget(account_name, saved_pin):
 def get_expenses(account_name, budget_month, total_budget):
     """
     Objective: Get the type, amount and transaction method
-    from the user and save them all to the Google sheet. 
-    
+    from the user and save them all to the Google sheet.
+
     Steps:
     1. Get the type of expense from the user.
     2. Get the type of expense amount from the user.
@@ -154,11 +154,11 @@ def get_expenses(account_name, budget_month, total_budget):
     today_date = datetime.date.today().strftime("%Y-%m-%d")
     # Expense types inputs
     print("Loading expense inputs...")
-    
+
     # Using a while loop to ask the user to add expenses until
     # they no longer want to add more.
     while True:
-        
+
         # A list of the different expense categories for
         # the user to choose from.
         expense_categories = [
@@ -168,7 +168,7 @@ def get_expenses(account_name, budget_month, total_budget):
             "Other",
             "Savings"
         ]
-        
+
         # Get the expense type from the user, using a while loop
         # to make sure the input is correct.
         while True:
@@ -186,15 +186,15 @@ def get_expenses(account_name, budget_month, total_budget):
                 print(f"You entered: {selected_expense_type}. "
                       f"Choose a number between 1 and "
                       f"{len(expense_categories)}.")
-                
+
         selected_expense_type = int(selected_expense_type)
-        
+
         # Get the name of the expense from the user.
         expense_name = input("Enter expense name: \n")
-        
+
         # Get the amount of the expense from the user.
         expense_amount = float(input("Enter expense amount: \n"))
-        
+
         # Get the transaction type from the user using a while loop to
         # ensure that the user enters a valid option.
         while True:
@@ -207,20 +207,20 @@ def get_expenses(account_name, budget_month, total_budget):
             else:
                 print(f"You must enter the details exactly as follows: "
                       "'debit' or 'credit'. Please try again")
-        
+
         # Prints the entered information for the user to see.
         print(f"You have entered {expense_name.capitalize()} at "
               f"{expense_amount}$, with {trans_type.capitalize()} and "
               f"category {expense_categories[selected_expense_type-1]}")
-        
-        # Saving the entered data to the worksheet. 
+
+        # Saving the entered data to the worksheet.
         # Get the number of rows in the worksheet
         row_count = len(budget_data.get_all_values())
         new_row = [account_name, budget_month.capitalize(), total_budget,\
             expense_name.capitalize(), expense_amount, \
             trans_type.capitalize(), today_date]
         budget_data.append_row(new_row, value_input_option="USER_ENTERED")
-        
+
         # Ask the user if they want to add another expense
         add_another = input("Do you want to add another expense? (y/n)\n")
         if add_another.lower() == "n":
@@ -233,18 +233,18 @@ def get_expenses(account_name, budget_month, total_budget):
 def calculate_budget(account_name, valid_months, budget_month):
     """
     Objective: Calculate the remaining budget for the user.
-    
+
     Steps:
     1. Match the users data to the month and date
     2. Calculate the remaining budget
     3. Display budget left in total, per day and how much
     credit is left to pay.
     """
-    
+
     # Get the user to choose a month to view their budget for.
     # Check so that the account name has data in the month that the user
     # chooses to see, if not, loops until the user chooses a month with data
-    # or the user quits the program.   
+    # or the user quits the program.
     print(f"All done {account_name}, You can now display your budget!\n")
     print(f"Choose the month you want to display your budget for:")
     print(f"You can choose between: {valid_months}\n")
@@ -268,40 +268,40 @@ def calculate_budget(account_name, valid_months, budget_month):
             print(f"That month does not exist. Make sure you choose between "
                 f"{valid_months}")
             return
-    
+
     # Set the total debit and Credit
     total_debit = 0
     total_credit = 0
     total_budget = 0
     left_per_day = 0
-    
+
     # Loop through the valid_budget_rows and get the total budget.
     for row in reversed(valid_budget_rows):
-            if row[2]:
-                total_budget += float(row[2])
-                break
-    
+        if row[2]:
+            total_budget += float(row[2])
+            break
+
     # Loop through the valid_budget_rows and sum up all debit expense amounts.
     for row in valid_budget_rows:
         if row[5] == "Debit":
             total_debit += float(row[4])
-    
+
     # Loop through the valid_budget_rows and sum up all credit expense amounts.
     for row in valid_budget_rows:
         if row[5] == "Credit":
-            total_credit += float(row[4])    
-    
+            total_credit += float(row[4])
+
     # Calculate how much the user has left
     total_left = total_budget - total_debit
-    
+
     # Get the number of days left in the month
     today_date = datetime.date.today()
     days_in_month = calendar.monthrange(today_date.year, today_date.month)[1]
     remaining_days = days_in_month - today_date.day
-    
+
     # Calculate how much the user has each day
     left_per_day = total_left / remaining_days
-    
+
     # Print the information to the user:
     print(f"Your Total Budget is: {total_budget:.2f}$.\n")
     print(f"You have a total of {total_left:.2f}$ left this month.\n")
@@ -310,7 +310,7 @@ def calculate_budget(account_name, valid_months, budget_month):
     print(f"You have {left_per_day:.2f}$ to spend per day"
           f" this month calulating that you need to\nsave"
           f" {total_credit:.2f}$ to afford the credit\n")
-    
+
 
 def delete_data(budget_data, account_name, valid_months):
     """
@@ -339,7 +339,7 @@ def delete_data(budget_data, account_name, valid_months):
                 deleted_rows += 1
         print(f"{deleted_rows} rows have been successfully deleted.")
 
-    
+
 def main():
     """
     A main function to call all functions of the program.
@@ -349,10 +349,10 @@ def main():
     get_expenses(account_name, budget_month, total_budget)
     calculate_budget(account_name, valid_months, budget_month)
     delete_data(budget_data, account_name, valid_months)
-    
+
 # Run the main function
 main()
 
 # New idea: Before the expenses, make the user choose to either enter
 # an expense, or see the budget. Maybe they only want to see earlier data and
-# not add new data. 
+# not add new data.
